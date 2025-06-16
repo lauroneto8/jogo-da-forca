fb03d110f33eb5397dac32cacd80691d47e2e582
lauroneto8 <almeida.neto.lauro@escola.pr.gov.br>
2025-06-16 08:53:55 -0300

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ let maxAttemps = 6;
 
 function startGame(){
     selectWord = words[Math.floor(Math.random9() * words.length)];
-
-
-
+    correctLetters = [];
+    wrongLetters = []
+    document.getElementById("message").textContent = "";
+    textContent = "";
+displayWord();
+generateLetterButtons()
 }
+function displayWord() {
+    const wordDiv = document.getElementById("word");
+    wordDiv.innerHTML = selectWord
+    .split("")
+    .map(letter => (correctLetters.includes(letter ? letter : "_")))
+    
+}
+
