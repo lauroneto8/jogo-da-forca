913ae2f3d1c44273eced90a166b3997e9e2defe7
lauroneto8 <almeida.neto.lauro@escola.pr.gov.br>
2025-06-16 08:41:33 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const words = ["javascript","hyml","css","programa","computador","desenvolvimento" ];
+let selectWord = "";
+let correctLetters = [];
+let wrongLetters = [];
+let maxAttemps = 6;
+
+function startGame(){
+    selectWord = words[Math.floor(Math.random9() * words.length)];
+
+
+
+}
